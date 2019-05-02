bcde3c155542e5ac3930cc66c6cc05beb366f910
harrisonfarrelluts <harrison.farrell@student.uts.edu.au>
2019-05-02 15:19:56 +1000

    progress

--- dobotClass.m
+++ dobotClass.m
@@ -18,11 +18,16 @@ classdef dobotClass
     function CreateDobot(self)
         pause(0.001); 
     name = ['Dobot',datestr(now,'yyyymmddTHHMMSSFFF')];
-    L1 = Link('d',0.15185,'a',0,'alpha',pi/2,'offset',0,'qlim', deg2rad([-135,135]));
-    L2 = Link('d',0,'a',-0.24355,'alpha',0,'offset',-pi/2, 'qlim', deg2rad([5,80])); 
-    L3 = Link('d',0,'a',-0.2132,'alpha',0,'offset',0, 'qlim', deg2rad([15,170])); %pi/2 –q2 actual offset
-    L4 = Link('d',0.11235,'a',0,'alpha',pi/2,'offset',0, 'qlim', [-pi/2,pi/2]); 
-    L5 = Link('d',0.08535,'a',0,'alpha',-pi/2,'offset',0, 'qlim', deg2rad([-85,85]));
+    %Base Angle = q1
+    %Rear Arm Angle = q2
+    %Fore Arm Angle = q3
+    %N/A 
+    %Servo Angle = q5
+    L1 = Link('d',0,'a',0,'alpha',0,'offset',0,'qlim', deg2rad([-135,135]));
+    L2 = Link('d',0,'a',0,'alpha',0,'offset',-pi/2, 'qlim', deg2rad([5,80]));     
+    L3 = Link('d',0,'a',0,'alpha',0,'offset',0, 'qlim', deg2rad([15,170])); %pi/2 –q2 actual offset    
+    L4 = Link('d',0,'a',0,'alpha',0,'offset',0, 'qlim', [-pi/2,pi/2]);     
+    L5 = Link('d',0,'a',0,'alpha',0,'offset',0, 'qlim', deg2rad([-85,85]));
     self.model = SerialLink([L1 L2 L3 L4 L5], 'name', name);
     end 
 
@@ -37,14 +42,14 @@ classdef dobotClass
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function gotoPoint(self,location,steps)     
     robotJoints = self.model.getpos()                               %getting the pose of the robot
-    newJoints = self.model.ikcon(location)                          %getting the pose of the robot  
-    jointMatrix = self.CalculateTrajectory(robotJoints, newJoints, steps); %creating a matrix of steps from current to endpoint
-    self.TransformLog(robotJoints,newJoints);                     %log robot's transform
-
-    for i = 1:steps                                                 %iterating throught the matrix
-       self.model.animate(jointMatrix(i,:)); 
-       pause(0.02); 
-    end
+    % finding the q1
+    l = sqrt(x^2+y^2); %the lenght to the endpoint in 2D space
+    theta = atan(y/x); %the rotation angle
+    % finding the q2 & q3
+    l = sqrt(x^2+y^2); %the lenght to the endpoint in 2D space
+    d = sqrt(x^2+y^2); %the lenght to the endpoint in 3D space
+    t1 = atan(z/l); %The angle between line D and sideTwo
+    t2 = acos((sideTwo^2 + d^2 - sideThree^2)/ 2*sideTwo*sideThree)
 end  
 
     end
