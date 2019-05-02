388da6020db9030fb79ad93de5fc73d058e41d5c
harrisonfarrelluts <harrison.farrell@student.uts.edu.au>
2019-05-02 13:22:24 +1000

    the beginnings to the dobotClass

--- dobotClass.m
+++ dobotClass.m
@@ -1,23 +1,52 @@
 classdef dobotClass
-    %UNTITLED2 Summary of this class goes here
-    %   Detailed explanation goes here
-    
     properties
-        Property1
+        model;
+        %simulation environment size
+        workspace = [-0.5 2 -0.5 -0.5 2]; 
     end
     
-    methods
-        function obj = untitled2(inputArg1,inputArg2)
-            %UNTITLED2 Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
-        end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
-        end
+    methods    
+%% Creating the Dobot and plotting into the workspace
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function self = DobotModel(location)
+        self.CreateDobot(); 
+        self.PlotRobot(location); 
+    end
+
+%% Creating the Dobot with all the links
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function CreateDobot(self)
+        pause(0.001); 
+    name = ['Dobot',datestr(now,'yyyymmddTHHMMSSFFF')];
+    L1 = Link('d',0.15185,'a',0,'alpha',pi/2,'offset',0,'qlim', deg2rad([-135,135]));
+    L2 = Link('d',0,'a',-0.24355,'alpha',0,'offset',-pi/2, 'qlim', deg2rad([5,80])); 
+    L3 = Link('d',0,'a',-0.2132,'alpha',0,'offset',0, 'qlim', deg2rad([15,170])); %pi/2 –q2 actual offset
+    L4 = Link('d',0.11235,'a',0,'alpha',pi/2,'offset',0, 'qlim', [-pi/2,pi/2]); 
+    L5 = Link('d',0.08535,'a',0,'alpha',-pi/2,'offset',0, 'qlim', deg2rad([-85,85]));
+    self.model = SerialLink([L1 L2 L3 L4 L5], 'name', name);
+    end 
+
+%% Plotting the Robot
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    function PlotRobot(self,location)
+    self.model.base = location; 
+    self.model.plot(self.qNeutral,'workspace',self.workspace,'noarrow','scale',0.1);
+    end
+
+%% goto Point
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function gotoPoint(self,location,steps)     
+    robotJoints = self.model.getpos()                               %getting the pose of the robot
+    newJoints = self.model.ikcon(location)                          %getting the pose of the robot  
+    jointMatrix = self.CalculateTrajectory(robotJoints, newJoints, steps); %creating a matrix of steps from current to endpoint
+    self.TransformLog(robotJoints,newJoints);                     %log robot's transform
+
+    for i = 1:steps                                                 %iterating throught the matrix
+       self.model.animate(jointMatrix(i,:)); 
+       pause(0.02); 
+    end
+end  
+
     end
 end
 
