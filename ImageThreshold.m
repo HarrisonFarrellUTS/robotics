5217ac3039136fa568b1550667c5dca64d09abf8
MasterDankey <39038167+MasterDankey@users.noreply.github.com>
2019-05-15 15:27:38 +1000

    AddedErodingandDilaing
    As above, also added a working image to avoid using camera for future stuffs

--- ImageThreshold.m
+++ ImageThreshold.m
@@ -8,74 +8,55 @@ classdef ImageThreshold < handle
     methods
         function self = ImageThreshold
             imaqhwinfo ('winvideo');
-            self.input = startCamera(self);
-            self.maskedImagePlayer = vision.VideoPlayer
-            self.binaryImagePlayer = vision.VideoPlayer
+            self.input = self.startCamera();
         end
         
         %% start camera
         function feed = startCamera(self)
             camlist = webcamlist;
-            cam = webcam(1)
+            cam = webcam(2);
             feed = cam;
-            % start(feed);
             preview(feed);
-        end      
+        end
         %% take snapshot
         function im = takeSnapshot(self)
             im = snapshot(self.input);
         end
-        
-        function binaryImage = thresholdImage(image, maskedImagePlayer, binaryImagePlayer)
-            
-            I = image;
-            
-            channel1Min = 100.000;
-            channel1Max = 255.000;
-            
-            % Define thresholds fora channel 2 based on histogram settings
-            channel2Min = 0.000;
-            channel2Max = 100.000;
-            
+        %% binarise Image
+        function binaryImage = thresholdImage(self, image)
+            I = rgb2lab(image);
+            % Define thresholds for channel 1 based on histogram settings
+            channel1Min = 0.000;
+            channel1Max = 100.000;
+            % Define thresholds for channel 2 based on histogram settings
+            channel2Min = 5.553;
+            channel2Max = 50.390;
             % Define thresholds for channel 3 based on histogram settings
-            channel3Min = 0.000;
-            channel3Max = 100.000;
-            
+            channel3Min = 9.863;
+            channel3Max = 45.189;
+            % Create mask based on chosen histogram thresholds
             sliderBW = (I(:,:,1) >= channel1Min ) & (I(:,:,1) <= channel1Max) & ...
                 (I(:,:,2) >= channel2Min ) & (I(:,:,2) <= channel2Max) & ...
                 (I(:,:,3) >= channel3Min ) & (I(:,:,3) <= channel3Max);
             BW = sliderBW;
-            
             % Initialize output masked image based on input image.
             maskedRGBImage = image;
             % Set background pixels where BW is false to zero.
             maskedRGBImage(repmat(~BW,[1 1 3])) = 0;
-            maskedRGBImage(repmat(BW,[1 1 3])) = 1;
-            
-            pause
-            maskedImagePlayer(maskedRGBImage)
-            
+            maskedRGBImage(repmat(BW,[1 1 3])) = 255;
+            % output the image
             binaryImage = maskedRGBImage;
             
-            
-            % if not black make white
-            
-            %
-            %  im = im2double(image);
-            %  [r c p] = size(im);
-            %  imR = squeeze(im(:,:,1));
-            %  imG = squeeze(im(:,:,2));
-            %  imB = squeeze(im(:,:,3));
-            %  imbinaryR = im2bw(imR,graythresh(imR));
-            %  imbinaryG = im2bw(imG,graythresh(imG));
-            %  imbinaryB = im2bw(imB,graythresh(imB));
-            %
-            %  binaryImage = imcomplement(imbinaryR&imbinaryG&imbinaryB);
-            %  binaryImagePlayer(binaryImage);
-            
-            
         end
-               
+        %% Errode and dilate image
+        function processedImage = erodeAndDilate(self, image)
+            SE = strel('square',5);
+            image = imdilate(image,SE);
+            
+            SE = strel('square',3);
+            processedImage = imerode(image,SE);
+        end
+        
     end
     
 end
---
+++ ScriptToRunthresholding.m
@@ -0,0 +1,31 @@
+
+
+clc
+close all
+clear all
+
+
+maskedImagePlayer = vision.VideoPlayer;
+binaryImagePlayer = vision.VideoPlayer;   
+erodeImagePlayer = vision.VideoPlayer;
+dilateImagePlayer = vision.VideoPlayer;
+
+imgur = ImageThreshold;
+disp('starting Loop');
+
+
+
+while(true)
+    
+image  = imgur.takeSnapshot();
+newimage = imgur.thresholdImage(image);
+
+processsedImage = imgur.erodeAndDilate(newimage);
+
+maskedImagePlayer(image);
+binaryImagePlayer(newimage);
+erodeImagePlayer(processsedImage);
+
+end
+
+%%
