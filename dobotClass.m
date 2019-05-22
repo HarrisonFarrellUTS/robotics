a8555ca3d0f1ad00222ce4e178a9ad05ad35286d
HarrisonFarrellUTS <harrison.farrell@student.uts.edu.au>
2019-05-22 11:16:14 +1000

    Requirement
    the requirement work is completed

--- dobotClass.m
+++ dobotClass.m
@@ -1,63 +1,65 @@
 classdef dobotClass < handle
     properties
         model;
-        %simulation environment size
+        simulation; 
         workspace = [-0.5 0.5 -0.5 0.5 -0.5 0.5]; 
         qNeutral = [0,deg2rad(45),deg2rad(90),deg2rad(45),0];
+        qSimulation = [0,deg2rad(45),deg2rad(90),deg2rad(45)];
     end
-    
     methods    
-%% Creating the Dobot and plotting into the workspace
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Constructor
     function self = dobotClass(location)
-        self.CreateDobot(); 
+        self.CreateDobot(location); 
+        %self.plotModel3d(location)
         %self.PlotRobot(location); 
+        %self.plotSimulation3d(location);
     end
 
-%% Creating the Dobot with all the links
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    function CreateDobot(self)
+%% Creating the Dobot both model with attachment and simulation
+    function CreateDobot(self, location)
+        
         pause(0.001); 
+        
         name = ['Dobot',datestr(now,'yyyymmddTHHMMSSFFF')];
-        %Base Angle = q1
-        %Rear Arm Angle = q2
-        %Fore Arm Angle = q3
-        %N/A 
-        %Servo Angle = q5
         L1 = Link('d',0.137,'a',0,'alpha',-pi/2,'offset',0,'qlim', deg2rad([-135 135]));
         L2 = Link('d',0,'a',0.1393,'alpha',0,'offset',-pi/2, 'qlim', deg2rad([5 80]));     
         L3 = Link('d',0,'a',0.16193,'alpha',0,'offset',0, 'qlim', deg2rad([15 170])); %pi/2 –q2 actual offset    
         L4 = Link('d',0,'a',0.0597,'alpha',pi/2,'offset',-pi/2, 'qlim', [-pi/2 pi/2]);     
-        L5 = Link('d',0,'a',0,'alpha',0,'offset',0, 'qlim', deg2rad([-85 85]));
-        
+        L5 = Link('d',0,'a',0,'alpha',0,'offset',0, 'qlim', deg2rad([-85 85]));       
         self.model = SerialLink([L1 L2 L3 L4 L5], 'name', name);
+        
+        pause(0.001);
+        
+        name = ['DobotMovementSimulation'];
+        L1 = Link('d',0.137,'a',0,'alpha',-pi/2,'offset',0,'qlim', deg2rad([-135 135]));
+        L2 = Link('d',0,'a',0.1393,'alpha',0,'offset',-pi/2, 'qlim', deg2rad([5 80]));     
+        L3 = Link('d',0,'a',0.16193,'alpha',0,'offset',0, 'qlim', deg2rad([15 170])); %pi/2 –q2 actual offset 
+        L4 = Link('d',0,'a',0.0597,'alpha',pi/2,'offset',-pi/2, 'qlim', [-pi/2 pi/2]); 
+        self.simulation = SerialLink([L1 L2 L3 L4], 'name', name);
+        
+        self.model.base = location;
+        self.simulation.base = location; 
+        
     end 
 
-%% Plotting the Robot
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Plot Stick figure of the robot 
     function PlotRobot(self,location)
     self.model.base = location; 
     self.model.plot(self.qNeutral); 
-    %self.model.plot(self.qNeutral,'workspace',self.workspace,'noarrow','scale',0.1);
     end
-%% PlotAndColourRobot
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Given a robot index, add the glyphs (vertices and faces) and
-% colour them in if data is available 
-function PlotAndColourRobot(self, location)%robot,workspace)
+%% Plot 3D model with attachment 
+function plotModel3d(self, location)
     for linkIndex = 0:self.model.n
-        [ faceData, vertexData, plyData{linkIndex + 1} ] = plyread(['dobotLink',num2str(linkIndex),'.ply'],'tri'); %#ok<AGROW>
+        [ faceData, vertexData, plyData{linkIndex + 1} ] = plyread(['dobotLink',num2str(linkIndex),'.ply'],'tri');
         self.model.faces{linkIndex + 1} = faceData;
         self.model.points{linkIndex + 1} = vertexData;
     end
-        % Display robot
         self.model.base = location; 
-        self.model.plot3d(self.qNeutral,'noarrow','workspace',self.workspace); %zeros(1,self.model.n)
+        self.model.plot3d(self.qNeutral,'noarrow','workspace',self.workspace);
         if isempty(findobj(get(gca,'Children'),'Type','Light'))
             camlight
         end  
     self.model.delay = 0;
-    % Try to correctly colour the arm (if colours are in ply file data)
     for linkIndex = 0:self.model.n
         handles = findobj('Tag', self.model.name);
         h = get(handles,'UserData');
@@ -72,28 +74,86 @@ function PlotAndColourRobot(self, location)%robot,workspace)
         end
     end
 end 
-%% goto Point
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function gotoPoint(self,location,steps)     
-    robotJoints = self.model.getpos()                               %getting the pose of the robot
-    % finding the q1
-    l = sqrt(x^2+y^2); %the lenght to the endpoint in 2D space
-    theta = atan(y/x); %the rotation angle
-    % finding the q2 & q3
-    l = sqrt(x^2+y^2); %the lenght to the endpoint in 2D space
-    d = sqrt(x^2+y^2); %the lenght to the endpoint in 3D space
-    t1 = atan(z/l); %The angle between line D and sideTwo
-    t2 = acos((sideTwo^2 + d^2 - sideThree^2)/ 2*sideTwo*sideThree)
+%% Plot 3D simulation Dobot - no attachments 
+function plotSimulation3d(self, location)
+    for linkIndex = 0:self.simulation.n
+        [ faceData, vertexData, plyData{linkIndex + 1} ] = plyread(['dobotLink',num2str(linkIndex),'.ply'],'tri');
+        self.simulation.faces{linkIndex + 1} = faceData;
+        self.simulation.points{linkIndex + 1} = vertexData;
+    end
+
+        self.simulation.base = location; 
+        self.simulation.plot3d(self.qSimulation,'noarrow','workspace',self.workspace);
+        if isempty(findobj(get(gca,'Children'),'Type','Light'))
+            camlight
+        end  
+    self.simulation.delay = 0;
+
+    for linkIndex = 0:self.simulation.n
+        handles = findobj('Tag', self.simulation.name);
+        h = get(handles,'UserData');
+        try 
+            h.link(linkIndex+1).Children.FaceVertexCData = [plyData{linkIndex+1}.vertex.red ...
+                                                          , plyData{linkIndex+1}.vertex.green ...
+                                                          , plyData{linkIndex+1}.vertex.blue]/255;
+            h.link(linkIndex+1).Children.FaceColor = 'interp';
+        catch ME_1
+            disp(ME_1);
+            continue;
+        end
+    end
 end 
-%% Calculate Trajectory
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function angleMatrix = CalculateTrajectory(self,robotA_q1,robotB_q2,steps)
-            scalar = lspb(0,1,steps);           
-            angleMatrix = nan(steps,6);        
-    for i = 1:steps
-        angleMatrix(i,:) = (1-scalar(i))*robotA_q1 + scalar(i)*robotB_q2;
+%% Dobot Requirement
+function requirement(self)
+    location = transl(0,0,0); 
+    plotSimulation3d(self, location)
+    load dobot_q
+    for i = 1:338
+        q1 = dobot_q(i,1);
+        q2 = dobot_q(i,2);
+        q3 = dobot_q(i,3);
+        q4 = pi - (q2 + q3); 
+        qMove = [q1,q2,q3,q4]; 
+       self.simulation.animate(qMove);
+       drawnow();
+       pause(0.05); 
     end
 end
+%% goto Point
+function goTo(self,location,steps)  
+    a2 = 0.1393;
+    a3 = 0.16193;
+    x = location(1,4)
+    y = location(2,4)
+    z = location(3,4)    
+    
+    robotJoints = self.model.getpos()                               
+
+    l = sqrt(x^2 + y^2); 
+    d = sqrt(l^2 + z^2);
+    
+    t1 = atan(z/l); 
+    t2 = acos(( (a2^2) + (d^2) - (a3^2) )/(2*d*a2));
+    
+    a = t1 + t2;
+    ad = rad2deg(a)
+    b = acos((a2^2) + (a3^2) - (d^2)/(2*a2*a3));
+    bd = rad2deg(b)
+    
+    q1r = atan2(y,x)
+    q2r = pi/2 - a
+    q3r = pi - b - a
+    q4r = pi - q3r - q2r
+    q5r = 0
+    
+    q1d = rad2deg(q1r) 
+    q2d = rad2deg(q2r)
+    q3d = rad2deg(q3r) 
+    q4d = rad2deg(q4r)
+    q5d = rad2deg(q5r)
+    
+    newJoints = [q1d,q2d,q3d,q4d,q5d]
+end 
     end
 end
 
--- main.m
+++ main.m
@@ -14,9 +14,18 @@ hold on;
 %creation of the one robot
 location = transl(0,0,0); 
 dobot = dobotClass(location); 
+point = transl(0.15,0.15,0); 
+dobot.requirement(); 
 
 %% 
-dobot.PlotAndColourRobot(location);
-%dobot.PlotRobot(location);
-dobot.model.teach();
-camlight();
+    load dobot_q
+    for i = 1:338
+        q1 = dobot_q(i,1);
+        q2 = dobot_q(i,2);
+        q3 = dobot_q(i,3);
+        q4 = pi - (q2 + q3); 
+        qMove = [q1,q2,q3,q4]; 
+       dobot.simulation.animate(qMove);
+       drawnow();
+       pause(0.08); 
+    end
