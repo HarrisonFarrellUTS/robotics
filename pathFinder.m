d60ee3dcd42a59f80e3b27200b5947d9650e81d7
MasterDankey <39038167+MasterDankey@users.noreply.github.com>
2019-05-23 15:19:25 +1000

    Cooorrected Path finding,
    now finds accrate cords and adds 999,999 when pen should lift

--- pathFinder.m
+++ pathFinder.m
@@ -3,130 +3,153 @@ classdef pathFinder < handle
     properties
         loadedImage = []
         coordStack = []
-        previouslyChecked = []
+        previouslyChecked
+        ImageHeight
+        ImageWidth
         
     end
     
-    methods        
-%         function self = loadImage(image)
-% %             self.loadedImage = image;
-%         end
+    methods
         
         function loadimage(self,image)
-        self.loadedImage = image    
+            self.loadedImage = image;
+            [self.ImageHeight, self.ImageWidth, ~] = size(self.loadedImage);
+            self.coordStack = [];
+            self.previouslyChecked = zeros(self.ImageHeight,self.ImageWidth);
         end
         
         
         function findPath(self)
-            [height, width, ~] = size(self.loadedImage);
-            self.coordStack = [];
-            self.previouslyChecked = zeros(height,width);
-            for i = 2: height-1
-                for  j = 2: width-1
+            
+            for i = 2:1:(self.ImageHeight-1)
+                for  j = 2:1:(self.ImageWidth-1)
                     if(self.previouslyChecked(i,j) == 0)
-                        if(checkcell(i,j))
-                            self.coordStack = [self.coordStack; i,j];
-                            checkSurronding(i,j);
+                        if(self.checkcell(i,j))
+                            self.coordStack = [self.coordStack ; i,j];
+                            self.checkSurronding(i,j);
                         end
                         self.previouslyChecked(i,j) = 1;
                     end
                 end
             end
-            self.previouslyChecked
-            self.coordStack
+            self.previouslyChecked;
         end
+        
         function result = checkcell(self,i,j)
-            image =self.loadedImage;
-            current = [image(i-1,j-1),image(i-1,j),image(i-1,j+1);...
-                image(i,j-1), image(i,j),image(i,j+1);...
-                image(i+1,j-1), image(i+1,j), image(i+1,j+1)];
-            result = 0;
-            if mean(current) > 141
-                result = 1;
+            if (~((i<2)||(i>self.ImageHeight)||(j<2)||(j>self.ImageWidth)))
+                image =self.loadedImage;
+                current = [image(i-1,j-1),image(i-1,j),image(i-1,j+1);...
+                    image(i,j-1), image(i,j),image(i,j+1);...
+                    image(i+1,j-1), image(i+1,j), image(i+1,j+1)];
+                result = 0;
+                if mean(current) > 141
+                    result = 1;
+                end
+            else
+                result = 0;
             end
             
+        end
+        
+        function checkSurronding(self,i,j)
+            path = 0;
+            self.previouslyChecked(i,j) = 1;
+            if(i>1)
+                if(j>1)
+                    x = i-1;
+                    y = j-1;
+                    if(self.previouslyChecked(x,y) == 0)
+                        if(self.checkcell(x,y))
+                            self.coordStack = [self.coordStack; x,y];
+                            path = 1;
+                            self.checkSurronding(x,y)
+                        end
+                        
+                    end
+                end
+                x = i-1;
+                y = j;
+                if(self.previouslyChecked(x,y) == 0)
+                    if(self.checkcell(x,y))
+                        self.coordStack = [self.coordStack; x,y];
+                        path = 1;
+                        self.checkSurronding(x,y)
+                    end
+                end
+                if(j>self.ImageWidth-1)
+                    x = i-1;
+                    y = j+1;
+                    if(self.previouslyChecked(x,y) == 0)
+                        if(self.checkcell(x,y))
+                            self.coordStack = [self.coordStack; x,y];
+                            path = 1;
+                            self.checkSurronding(x,y)
+                        end
+                    end
+                end
+            end
+            if(j>1)
+                x = i;
+                y = j-1;
+                if(self.previouslyChecked(x,y) == 0)
+                    if(self.checkcell(x,y))
+                        self.coordStack = [self.coordStack; x,y];
+                        path = 1;
+                        self.checkSurronding(x,y)
+                    end
+                end
+            end
             
-            function checkSurronding(self,i,j)
-                path = 0;
-                x = i-1;
-                y = j-1;
-                if(self.previouslychecked(x,y) == 0)
-                    if(checkcell(x,y))
-                        self.coordStack = [self.coordStack; x,y];
-                        path = 1;
-                    end
-                    checkSurronding(x,y)
-                end
-                x = i-1;
-                y = j;
-                if(self.previouslychecked(x,y) == 0)
-                    if(checkcell(x,y))
-                        self.coordStack = [self.coordStack; x,y];
-                        path = 1;
-                    end
-                    checkSurronding(x,y)
-                end
-                
-                x = i-1;
-                y = j+1;
-                if(previouslychecked(x,y) == 0)
-                    if(checkcell(x,y))
-                        self.coordStack = [self.coordStack; x,y];
-                        path = 1;
-                    end
-                    checkSurronding(x,y)
-                end
-                x = i;
-                y = j-1;
-                if(previouslychecked(x,y) == 0)
-                    if(checkcell(x,y))
-                        self.coordStack = [self.coordStack; x,y];
-                        path = 1;
-                    end
-                    checkSurronding(x,y)
-                end
-                
+            if(j>self.ImageWidth-1)
                 x = i;
                 y = j+1;
-                if(previouslychecked(x,y) == 0)
-                    if(checkcell(x,y))
+                if(self.previouslyChecked(x,y) == 0)
+                    if(self.checkcell(x,y))
                         self.coordStack = [self.coordStack; x,y];
                         path = 1;
+                        self.checkSurronding(x,y)
                     end
-                    checkSurronding(x,y)
+                end
+            end
+            
+            if(i<self.ImageHeight-1)
+                if(j>1)
+                    x = i+1;
+                    y = j-1;
+                    if(self.previouslyChecked(x,y) == 0)
+                        if(self.checkcell(x,y))
+                            self.coordStack = [self.coordStack; x,y];
+                            path = 1;
+                            self.checkSurronding(x,y)
+                        end
+                    end
                 end
                 
                 x = i+1;
-                y = j-1;
-                if(previouslychecked(x,y) == 0)
-                    if(checkcell(x,y))
-                        self.coordStack = [self.coordStack; x,y];
-                        path = 1;
-                    end
-                    checkSurronding(x,y)
-                end
-                x = i+1;
                 y = j;
-                if(previouslychecked(x,y) == 0)
-                    if(checkcell(x,y))
+                if(self.previouslyChecked(x,y) == 0)
+                    if(self.checkcell(x,y))
                         self.coordStack = [self.coordStack; x,y];
                         path = 1;
+                        self.checkSurronding(x,y)
                     end
-                    checkSurronding(x,y)
                 end
-                x = i+1;
-                y = j+1;
-                if(previouslychecked(x,y) == 0)
-                    if(checkcell(x,y))
-                        self.coordStack = [self.coordStack; x,y];
-                        path = 1;
-                    end
-                    checkSurronding(x,y)
-                end                
-                if path == 0
-                    self.coordStack = [self.coordStack ; [999 999]];
-                end                
+                if(j < self.ImageWidth-1)
+                    x = i+1;
+                    y = j+1;
+                    if(self.previouslyChecked(x,y) == 0)
+                        if(self.checkcell(x,y))
+                            self.coordStack = [self.coordStack; x,y];
+                            path = 1;
+                            self.checkSurronding(x,y)
+                        end
+                    end                
+                end
+            end
+            if(path == 0)
+                self.coordStack = [self.coordStack ; [999 999]];
             end
         end
+        
     end
 end
