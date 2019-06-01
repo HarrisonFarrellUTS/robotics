ed35b27a987914cd2030f725999247c5f72a3bb3
MasterDankey <39038167+MasterDankey@users.noreply.github.com>
2019-06-01 13:08:27 +1000

    Increased rad of drawn cells,
    now does a 7x7 grid whenever a pixel is drawn ,should futher incerease rate of drawing.

--- ImageThreshold.m
+++ ImageThreshold.m
@@ -53,7 +53,7 @@ classdef ImageThreshold < handle
 %            SE = strel('square',2);
 %             image = imerode(image,SE);
             
-            SE = strel('square',2);
+            SE = strel('square',5);
             processedImage = imdilate(image,SE);
             
 %             SE = strel('square',5);
